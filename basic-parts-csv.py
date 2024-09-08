a6e92d53acabbb8d7ff567d98f59f6ff154a41c3
CDFER <cdirksfer@gamil.com>
2024-09-08 15:58:21 +1200

    shrink sqlite database size and try to upload it to github pages

--- basic-parts-csv.py
+++ basic-parts-csv.py
@@ -2,27 +2,40 @@ import pandas as pd
 import sqlite3
 import os
 
-# TODO Exclude 0201 components?
+os.chdir("db_build")
 
-output_directory = "db_build"
-os.chdir(output_directory)
+initial_db_size = os.path.getsize("cache.sqlite3")
+print(f"Initial SQLite Database Size: {initial_db_size / (1024 ** 3):.2f} GiB")
 
 conn = sqlite3.connect("cache.sqlite3")
 cur = conn.cursor()
-cur.execute("DELETE FROM components WHERE stock <= 0;")
-conn.commit()
-print(f"Deleted {cur.rowcount} components that aren't in stock")
 
-cur = conn.cursor()
+# Delete components with low stock
+cur.execute("DELETE FROM components WHERE stock < 5;")
+conn.commit()
+print(f"Deleted {cur.rowcount} components with low stock")
+
+# Vacuum database to reduce file size
+cur.execute("VACUUM;")
+conn.commit()
+
+optimized_db_size = os.path.getsize("cache.sqlite3")
+print(f"Optimized Database Size: {optimized_db_size / (1024 ** 3):.2f} GiB")
+
+# Retrieve basic/preferred components ($0 for loading feeders)
 cur.execute(
-    "SELECT * FROM v_components WHERE (basic > 0 OR preferred > 0) AND stock >= 10;"
+    """
+    SELECT * FROM v_components 
+    WHERE (basic > 0 OR preferred > 0);
+"""
 )
 filtered_components = cur.fetchall()
 
+# Create Pandas DataFrame and sort components
 df = pd.DataFrame(filtered_components, columns=[desc[0] for desc in cur.description])
-df_sorted = df.sort_values(by=["category", "subcategory"])
-df_sorted.to_csv(
-    "BasicOrPreferredPartsList.csv", index=False, header=True
-)
+df_sorted = df.sort_values(by=["category", "subcategory", "package"])
+
+# Save sorted DataFrame to CSV
+df_sorted.to_csv("BasicOrPreferredPartsList.csv", index=False, header=True)
 
 conn.close()
